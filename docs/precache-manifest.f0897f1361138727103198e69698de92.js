794a65bdf7d48c3f33315282fe57db66c863e3f3
bhoomij <bhoomijoshi31@gmail.com>
2020-02-04 16:48:21 +0530

    Add build-storybook

---
+++ docs/precache-manifest.f0897f1361138727103198e69698de92.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9afa5c0e68f38cdb6b3caad4ef621a5",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.cbc94f04068c5ac43e24.bundle.js"
+  },
+  {
+    "url": "runtime~main.cbc94f04068c5ac43e24.bundle.js"
+  },
+  {
+    "url": "vendors~main.cbc94f04068c5ac43e24.bundle.js"
+  },
+  {
+    "url": "vendors~main.cbc94f04068c5ac43e24.bundle.js.LICENSE.txt"
+  }
+]);
